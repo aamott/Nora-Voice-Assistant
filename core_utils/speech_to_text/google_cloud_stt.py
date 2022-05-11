403fb4d2e907bc5bb293e363d81c24e45c4cb408
aamott <adam.amott@gmail.com>
2022-05-10 19:35:36 -0600

    Added free google STT

--- core_utils/speech_to_text/google_cloud_stt.py
+++ core_utils/speech_to_text/google_cloud_stt.py
@@ -1,23 +1,50 @@
 #######################################
-# Google Cloud Speech
-# Requires:
-#     pip install google-cloud-speech
+# Google Cloud Speech-to-Text
 #######################################
-from google.cloud import speech
+from core_utils.speech_to_text.stt_abstract import STT as STT_Abstract
+from core_utils.core_core.audio_recorder import AudioRecorder
+import speech_recognition as sr
 
 
-class Google_STT:
+class STT( STT_Abstract ):
     # The id of the object as it will appear in the json
     name = "Google_STT"
 
     def __init__(self, credentials_file='credentials.json'):
-        self.client = speech.SpeechClient.from_service_account_json(
-            credentials_file)
+        self.audio_recorder = AudioRecorder()
+        self.recognizer = sr.Recognizer()
+
+
+    def calibrate_audio(self):
+        """
+        Calibrates the audio recorder
+        """
+        self.audio_recorder.calibrate_silence()
+
 
     def listen(self):
         """
         Listens for audio, then returns the text of the audio
         """
         # record
-        with self.mic as source:
-            audioData = self.sr.listen(source)
+        audio_file = self.audio_recorder.get_recording_as_wav()
+
+        # convert to AudioData object
+        # Obtained from https://stackoverflow.com/questions/61961587/can-i-do-recognition-from-numpy-array-in-python-speechrecognition
+        audio_data = sr.AudioData(audio_file.read(), 48000, 2)
+
+        # recognize
+        try:
+            text = self.recognizer.recognize_google(audio_data)
+            print("Done listening")
+            return text
+
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+            return None
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            return None
+
+        # with self.mic as source:
+        #     audioData = self.sr.listen(source)
