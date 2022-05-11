3a1b36c2d0b5b4ba34aef1a0244cfba6074792da
aamott <adam.amott@gmail.com>
2022-05-10 20:55:02 -0600

    Reformatted for easier testing

--- nora.py
+++ nora.py
@@ -1,9 +1,10 @@
+from time import sleep
 import core_utils.speech_to_text_getter as stt_getter
 import core_utils.intent_parser as intent_parser
 import core_utils.skill_creator as skill_creator
 
-# get the STT object
-stt = stt_getter.get_stt_object()
+# get the STT object - can currently be 'google' or 'deepspeech'
+stt = stt_getter.get_stt_object(stt_type="google")
 
 # import the skills
 skills = skill_creator.import_skills()
@@ -11,15 +12,24 @@ skills = skill_creator.import_skills()
 # initialize the intent parser
 intent_parser = intent_parser.IntentParser(skills)
 
+# calibrating
+print("Calibrating...")
+stt.calibrate_audio()
+
 ########################################
 # main loop
 ########################################
 while True:
+    # TODO: Remove when wakeword is implemented
+    sleep(1)
+
     # get the audio
-    audio = stt.listen()
+    print("Listening...")
+    text = stt.listen()
+    print("You said:", text)
 
     # get the intent
-    intent = intent_parser.parse_intent(audio)
+    intent = intent_parser.parse_intent(text)
 
     # run the intent
-    intent()
+    # intent()
