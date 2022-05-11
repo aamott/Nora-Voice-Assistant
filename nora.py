38ce748d43de4a4d25b369d3f2c2ec119a4db2f0
aamott <adam.amott@gmail.com>
2022-05-11 12:28:11 -0600

    Start using tts

--- nora.py
+++ nora.py
@@ -1,10 +1,15 @@
 from time import sleep
 import core_utils.speech_to_text_getter as stt_getter
+import core_utils.text_to_speech_getter as tts_getter
 import core_utils.intent_parser as intent_parser
 import core_utils.skill_creator as skill_creator
 
+
 # get the STT object - can currently be 'stub', 'google', 'deepspeech'
 stt = stt_getter.get_stt_object(stt_type="stub")
+
+# get the TTS object - can currently be 'print', 'google', 'pyttsx3'
+tts = tts_getter.get_tts_object(tts_type="pyttsx3")
 
 # import the skills
 skills = skill_creator.import_skills()
@@ -19,14 +24,15 @@ stt.calibrate_audio()
 ########################################
 # main loop
 ########################################
+tts.say("Hello, I am Nora. I am a virtual assistant.")
 while True:
     # TODO: Remove when wakeword is implemented
-    sleep(1)
+    sleep(5)
 
     # get the audio
     print("Listening...")
     text = stt.listen()
-    print("You said:", text)
+    tts.say("You said: " + text)
 
     # get the intent
     intent = intent_parser.parse_intent(text)
@@ -35,4 +41,4 @@ while True:
     if intent is not None:
         intent["callback"](intent)
     else:
-        print("No intent detected")
+        tts.say("No intent detected")
