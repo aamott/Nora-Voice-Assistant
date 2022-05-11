d9513360ebbeee534b2d27a031d2aa5768cdd227
aamott <adam.amott@gmail.com>
2022-05-10 19:36:17 -0600

    Updated stub for Abstract STT

--- core_utils/speech_to_text/stub_stt.py
+++ core_utils/speech_to_text/stub_stt.py
@@ -2,14 +2,17 @@
 # Stub Speech to Text
 #  Acts as a stand-in for the real STT module
 #######################################
+from core_utils.speech_to_text.stt_abstract import STT as STT_Abstract
 
-
-class STT:
+class STT( STT_Abstract ):
     # The id of the object as it will appear in the json
     name = "Stub_STT"
 
     def __init__(self):
         # Initialize stuff for speech recognition
+        pass
+
+    def calibrate_audio(self):
         pass
 
     def listen(self):
