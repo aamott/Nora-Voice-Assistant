b1665a385ccefa2d92947bfd8ae6f99c355d7119
aamott <adam.amott@gmail.com>
2022-05-10 21:07:40 -0600

    Added scripts to fetch deepspeech models

---
+++ core_utils/speech_to_text/deepspeech/fetch-models.ps1
@@ -0,0 +1,4 @@
+# Open the terminal from this folder and run this file (or copy and paste it in the terminal)
+# The format is: wget <file url> -O <filename>
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm -O deepspeech-0.9.3-models.pbmm
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer -O deepspeech-0.9.3-models.scorer
