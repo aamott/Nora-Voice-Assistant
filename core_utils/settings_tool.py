56bdeb04ff8c664f690714b8c72eca08eb3473e0
aamott <adam.amott@gmail.com>
2022-05-14 09:45:08 -0600

    Added settings tool

---
+++ core_utils/settings_tool.py
@@ -0,0 +1,57 @@
+#########################
+# Settings Tool
+# Wrapper for the settings manager
+# Passed into skills to allow them to access settings.
+##########################
+from core_utils.core_core.settings_manager import SettingsManager
+
+
+class SettingsTool:
+
+    def __init__(self, settings_manager : SettingsManager, setting_path: str) -> None:
+        """ Initializes the settings tool
+
+        Args:
+            settings_manager (SettingsManager): The settings manager
+            setting_path (str): The path to the setting subsection (ex. speech.stt.google). 
+                                        Changes to settings will only be made in this subsection.
+        """
+        self.settings_manager = settings_manager
+        self.setting_path = setting_path
+
+
+    def get_setting(self, keys) -> object:
+        """ Gets the value of the setting using a path
+            Parameters:
+                keys (string): path to the setting, separated by "."
+                                For example, "speech.stt.google.credentials"
+            
+            Returns: dict or single value 
+        """
+        return self.settings_manager.get_setting(self.setting_path + "." + keys)
+
+
+    def set_setting(self, setting_path, value) -> bool:
+        """ Sets the value of the setting using a path
+            Parameters:
+                setting_path (string): path to the setting, separated by "."
+                                                For example, "speech.stt.google.credentials"
+                value (any): value to set 
+                
+                Returns:
+                    bool: True if the setting was set, False otherwise 
+        """
+        return self.settings_manager.set_setting(self.setting_path + "." + setting_path, value)
+
+
+
+if __name__ == "__main__":
+
+    # Setup
+    settings_manager = SettingsManager()
+    settings_manager.__init__()
+    settings_tool = SettingsTool(settings_manager, "speech.stt.google")
+    # Test
+    print(settings_tool.get_setting("credentials"))
+    settings_tool.set_setting("credentials", "test credentials")
+    print(settings_tool.get_setting("credentials"))
