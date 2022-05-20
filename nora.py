1ce760f67e281c927042bb142d1aa5939aaa2710
aamott <adam.amott@gmail.com>
2022-05-20 01:19:06 -0600

    Implemented wakeword

--- nora.py
+++ nora.py
@@ -8,6 +8,7 @@ from core_utils.settings_tool import SettingsTool
 import core_utils.intent_parser as intent_parser
 import core_utils.skill_creator as skill_creator
 from core_utils.audio_utils import AudioUtils
+from core_utils.wakeword import Wakeword
 
 
 #######################
@@ -15,8 +16,14 @@ from core_utils.audio_utils import AudioUtils
 #######################
 settings_manager = SettingsManager()
 
+# initialize audio utilities
 audio_utils_settings_tool = SettingsTool(settings_manager, setting_path="audio utils")
 audio_utils = AudioUtils(settings_tool=audio_utils_settings_tool)
+
+# calibrating audio
+print("Calibrating...")
+audio_utils.calibrate_silence()
+print("Calibration complete!")
 
 # import the skills
 skills = skill_creator.import_skills(settings_manager=settings_manager)
@@ -27,10 +34,35 @@ intent_settings_tool = SettingsTool(settings_manager=settings_manager,
 intent_parser = intent_parser.IntentParser(skills,
                                            settings_tool=intent_settings_tool)
 
-# calibrating
-print("Calibrating... Shhh...")
-audio_utils.calibrate_silence()
-print("Calibrated!")
+# main callback
+def loop():
+    """ Main Callback. Processes the user's input and starts a skill."""
+    # TODO: play a sound and light up a few RGB LEDs
+
+    # listen for the user's response
+    print("Listening...")
+    text = audio_utils.listen()
+    audio_utils.say("You said: " + text)
+
+    # get the intent
+    intent_data = intent_parser.parse_intent(text)
+
+    # run the intent
+    if intent_data is not None:
+        print("Running intent: " + intent_data["name"])
+        intent_data["callback"](intent_data)
+    else:
+        audio_utils.say("No intent detected")
+
+
+# initialize wakeword
+wakeword_settings_tool = SettingsTool(settings_manager=settings_manager,
+                                        setting_path='wakeword.picovoice')
+wakeword = Wakeword(settings_tool=wakeword_settings_tool,
+                    audio_utils=audio_utils,
+                    wakeword_detected_callback=loop)
+
+
 
 
 ########################################
@@ -38,19 +70,8 @@ print("Calibrated!")
 ########################################
 audio_utils.say("Hello, I am Nora. I am a virtual assistant.")
 while True:
-    # TODO: Remove when wakeword is implemented
-    sleep(5)
-
-    # get the audio
-    print("Listening...")
-    text = audio_utils.listen()
-    audio_utils.say("You said: " + text)
-
-    # get the intent
-    intent = intent_parser.parse_intent(text)
-
-    # run the intent
-    if intent is not None:
-        intent["callback"](intent)
-    else:
-        audio_utils.say("No intent detected")
+    # wait for wakeword
+    wakeword.await_wakeword()
+    # loop()
+    # sleep(10)
+    print("Wakeword detected. Looping again!\n")
