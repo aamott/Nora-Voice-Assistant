0f5d43ec5b71b3d483f806458f73a0011d78c71d
aamott <adam.amott@gmail.com>
2022-05-27 11:54:17 -0600

    Comments and nav updates

--- core_utils/server.py
+++ core_utils/server.py
@@ -1,6 +1,8 @@
 ################################
 # Web Server
 # https://fastapi.tiangolo.com/tutorial/first-steps/
+# Starting and stopping the server: 
+#       https://stackoverflow.com/questions/61577643/python-how-to-use-fastapi-and-uvicorn-run-without-blocking-the-thread
 ################################
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
