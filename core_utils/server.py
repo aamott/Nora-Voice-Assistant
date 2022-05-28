a5470ce530f61c403991e672623e71cbeeb7831b
aamott <adam.amott@gmail.com>
2022-05-28 10:52:41 -0600

    cleaner setup

--- core_utils/server.py
+++ core_utils/server.py
@@ -4,6 +4,7 @@
 # Starting and stopping the server:
 #       https://stackoverflow.com/questions/61577643/python-how-to-use-fastapi-and-uvicorn-run-without-blocking-the-thread
 ################################
+from socket import create_server
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
@@ -14,6 +15,24 @@ import threading
 import uvicorn
 
 from core_core import channels, settings_manager
+
+
+class Server(uvicorn.Server):
+
+    def install_signal_handlers(self):
+        pass
+
+    @contextlib.contextmanager
+    def run_in_thread(self):
+        thread = threading.Thread(target=self.run)
+        thread.start()
+        try:
+            while not self.started:
+                time.sleep(1e-3)
+            yield
+        finally:
+            self.should_exit = True
+            thread.join()
 
 
 def create_app(channels: channels.Channels,
@@ -39,32 +58,27 @@ def create_app(channels: channels.Channels,
     return app
 
 
-class Server(uvicorn.Server):
 
-    def install_signal_handlers(self):
-        pass
 
-    @contextlib.contextmanager
-    def run_in_thread(self):
-        thread = threading.Thread(target=self.run)
-        thread.start()
-        try:
-            while not self.started:
-                time.sleep(1e-3)
-            yield
-        finally:
-            self.should_exit = True
-            thread.join()
+def create_server(channels: channels.Channels,
+                    settings_manager: settings_manager.SettingsManager):
+    app = create_app(channels=channels, settings_manager=settings_manager)
+
+    config = uvicorn.Config(app=app,
+                            host="127.0.0.1",
+                            port=5000,
+                            log_level="info")
+    server = Server(config=config)
+
+    return server
 
 
 
 if __name__ == "__main__":
+    channels = channels.Channels()
+    settings_manager = settings_manager.SettingsManager(settings_file="settings.yaml")
     # Launch the server
-    app = create_app(
-        channels.Channels(),
-        settings_manager.SettingsManager(settings_file="settings.yaml"))
-    config = uvicorn.Config(app=app, host="127.0.0.1", port=5000, log_level="info")
-    server = Server(config=config)
+    server = create_server(channels=channels, settings_manager=settings_manager)
 
     with server.run_in_thread():
         while True:
