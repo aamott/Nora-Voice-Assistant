31f905b412ec933f871e3d7e8015a4bec332b339
aamott <adam.amott@gmail.com>
2022-05-29 16:02:15 -0600

    added parameter checking

--- core_utils/core_core/channels.py
+++ core_utils/core_core/channels.py
@@ -6,44 +6,67 @@
 #   - publish(message, channel_name)
 #   - subscribe(callback, channel_name)
 #################################################
+from inspect import signature
 
 class Channels:
     def __init__(self):
         self.channels = {}
 
-    def publish(self, message: str, channel_name: str):
+    def publish(self, message, channel_name: str):
         """ Publish a message to a channel.
+            Before calling a callback, the message is checked to see if it matches the signature of the callback.
+            If it does not, the callback is not called and execution continues.
 
         Args:
-            message (str): The message to be published.
+            message: The message to be published. Can be a string, a list, a number, etc.
             channel_name (str): The name of the channel to publish to.
         """
-        if channel_name in self.channels:
-            for callback in self.channels[channel_name]:
-                callback(message)
+        channel = self.channels.get(channel_name)
+        if channel:
+            for callback in channel:
+                # Check if message is of the same type as the callback's first parameter
+                sig = signature(callback)
+                if sig.parameters[0].annotation == type(message):
+                    callback(message)
 
 
-    def subscribe(self, callback: callable, channel_name: str):
+    def subscribe(self, callback: callable, channel_name: str) -> bool:
         """ Subscribe to a channel.
 
         Args:
-            callback (callable): The callback callable to be called when a message is published to the channel.
+            callback (callable): The callback to be called when a message is published to the channel.
+                                        Must have only one parameter, which is the message.
             channel_name (str): The name of the channel to subscribe to.
+        Returns:
+            bool: True if the callback was successfully subscribed to the channel, False otherwise.
         """
         if channel_name not in self.channels:
             self.channels[channel_name] = []
-        self.channels[channel_name].append(callback)
+        
+        # Check that the callback has only one parameter
+        sig = signature(callback)
+        if len(sig.parameters) != 1:
+            return False
+        else:
+            self.channels[channel_name].append(callback)
+            return True
 
 
-    def unsubscribe(self, callback: callable, channel_name: str):
+    def unsubscribe(self, callback: callable, channel_name: str) -> bool:
         """ Unsubscribe from a channel.
 
         Args:
-            callback (callable): The callback callable to be removed from the channel.
+            callback (callable): The callback to be removed from the channel.
             channel_name (str): The name of the channel to unsubscribe from.
+
+        Returns:
+            bool: True if the callback was unsubscribed from the channel, False if the callback was not subscribed to the channel.
         """
         if channel_name in self.channels:
             self.channels[channel_name].remove(callback)
+            return True
+        else:
+            return False
 
 
     def get_channels(self):
