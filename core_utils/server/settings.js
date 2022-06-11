0cecfeae32ea54a6e8ed764f80c122db3da3b35c
aamott <adam.amott@gmail.com>
2022-06-10 18:03:04 -0600

    Add save button, better element grouping

--- core_utils/server/settings.js
+++ core_utils/server/settings.js
@@ -50,11 +50,15 @@ function populateSettings(settings) {
  * @param {string} settings_path 
  * @param {string} name 
  * @param {HTMLElement} parent 
+ * @returns {[HTMLElement, bool]} html element, is element a group
  */
 // Make a function that will fetch all the settings and populate the page
 function initSettings(setting, settings_path='', name='', parent=null) {
+    const IS_GROUP = true;
+    const IS_SETTING = false;
+
     if (!setting || !parent) {
-        return;
+        return [null, IS_GROUP];
     }
 
     // settingElement will either be made into a group (div) or a setting (input/label)
@@ -72,17 +76,52 @@ function initSettings(setting, settings_path='', name='', parent=null) {
         // Add group to parent
         parent.appendChild( group );
 
+        // Stores the elements for a form at the end of the group
+        let form_elements = [];
+
         for (key in setting) {
             // Group is passed by reference. This will add a bunch of child elements to the group.
-            initSettings( setting[ key ], settings_path + '.' + key, name = key, parent = group );
+            let element, is_group;
+            [element, is_group] = initSettings( setting[ key ], settings_path + '.' + key, /*name*/ key, /*parent*/ group );
+
+            // only add the element to the form if it's not a group
+            if ( !is_group && typeof element === 'object' ) {
+                console.log("here");
+                form_elements.push( element );
+            }
         }
+
+        // Add all the elements to a form if there are any
+        if ( form_elements.length > 0 ) {
+            let form = document.createElement('form');
+            form.classList.add('settings-form');
+            form.innerHTML = `<h4>${name}</h4>`;
+            
+            
+            // Add all the elements to the form
+            for (element of form_elements) {
+                form.appendChild( element );
+            }
+
+            // submit button
+            form.innerHTML += `<button type="submit">Save</button>`;
+
+            
+            // Add the form to the group
+            group.appendChild( form );
+        }
+
+
+        return [group, IS_GROUP];
 
     // It's a setting
     } else {
         settingElement = getSettingsElement(setting, settings_path, name);
 
         // Add the setting to the parent
-        parent.appendChild( settingElement );
+        // parent.appendChild( settingElement );
+
+        return [settingElement, IS_SETTING];
     }
 }
 
