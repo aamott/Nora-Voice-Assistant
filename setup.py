a3f68c9f630f3c0ba3ce3974731cc2d8c2efc2d9
aamott <adam.amott@gmail.com>
2022-06-28 17:53:22 -0600

    Move setup to py file

--- setup.py
+++ setup.py
@@ -7,15 +7,75 @@ import os
 from passlib.context import CryptContext
 import yaml
 
-def make_user():
-    # Check if user.yaml exists:
+
+def create_settings_file():
+    filepath = "./settings.yaml"
+
+    # Check if settings.yaml exists:
     overwrite = False
     skip = False
-    filepath = "./core_utils/server_resources/database/users.yaml"
     if os.path.exists(filepath):
         # check if the user wants to overwrite the file:
         while True:
-            user_input = input("The file already exists. Do you want to skip user creation, overwrite the file, or append to it?\n ([s]kip/[o]verwrite/[A]ppend) ").lower()
+            user_input = input("The settings file already exists. Do you want to skip settings creation, overwrite the file, or append to it?\n([s]kip/[o]verwrite/[A]ppend) ").lower()
+            user_input = user_input if user_input else "a"
+            if user_input == "o":
+                overwrite = True
+                break
+            elif user_input == "s":
+                overwrite = False
+                break
+            elif user_input == "a":
+                overwrite = False
+                break
+
+    # If the file does not exist or the user wants to overwrite it:
+    if skip:
+        print("Skipping settings creation.")
+    else:
+        print("Please go to the following URL to obtain an access key")
+        print("https://console.picovoice.ai/")
+
+        # Gather user input:
+        access_key = input("Access Key: ")
+        access_key = access_key if access_key else None
+
+        # Create the user dictionary:
+        settings = {
+            "wakeword": {
+                "picovoice": {
+                    "key": access_key,
+                    "keywords": ['jarvis'],
+                    "model_path": None,
+                    "sensitivities": None
+                }
+            }
+        }
+
+        if overwrite:
+            # Create the file:
+            with open(filepath, "w+") as file:
+                yaml.safe_dump(settings, file)
+        else:
+            # Append the user to the file:
+            with open(filepath, "a+") as file:
+                yaml.safe_dump(settings, file)
+                file.write("\n")
+
+            print("settings.yaml created.")
+    print()
+
+
+def make_user():
+    filepath = "./core_utils/server_resources/database/users.yaml"
+
+    # Check if user.yaml exists:
+    overwrite = False
+    skip = False
+    if os.path.exists(filepath):
+        # check if the user wants to overwrite the file:
+        while True:
+            user_input = input("The users file already exists. Do you want to skip user creation, overwrite the file, or append to it?\n([s]kip/[o]verwrite/[A]ppend) ").lower()
             user_input = user_input if user_input else "a"
             if user_input == "o":
                 overwrite = True
@@ -60,6 +120,9 @@ def make_user():
                 yaml.safe_dump(all_users, file)
                 file.write("\n")
 
-            print("User created.")
+            print("users.yaml created.")
+    print()
 
+
+create_settings_file()
 make_user()
