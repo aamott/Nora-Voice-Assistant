8e2be8e514933971c12d795eabffce68ff3f38c4
aamott <adam.amott@gmail.com>
2022-06-30 16:58:08 -0600

    fixed incorrect indentation

--- skills/internet_search_skill.py
+++ skills/internet_search_skill.py
@@ -10,35 +10,35 @@ import webbrowser
 class Skill(base_skill.BaseSkill):
     name = "Internet Search Skill"
 
-def __init__(self, settings_tool: SettingsTool, channels: Channels, audio_utils: AudioUtils):
-    self.settings_tool = settings_tool
-    self.channels = channels
-    self.audio_utils = audio_utils
+    def __init__(self, settings_tool: SettingsTool, channels: Channels, audio_utils: AudioUtils):
+        self.settings_tool = settings_tool
+        self.channels = channels
+        self.audio_utils = audio_utils
 
 
-def intent_creator(self, register_intent: callable):
-    """ This function registers intents using register_intent.
-        Parameters: 
-            register_intent (callable): a function that can add an intent to Nora. """
+    def intent_creator(self, register_intent: callable):
+        """ This function registers intents using register_intent.
+            Parameters: 
+                register_intent (callable): a function that can add an intent to Nora. """
 
-    # register the internet search intent
-    register_intent(
-        intent_callback=self.internet_search_intent,
-        # We can add 'entities', (or named values to look for), to the intent.
-        intent_phrases=[
-            "Search (for | ) {search_phrase}", "Look up {search_phrase}"
-        ],
-        intent_name="Internet_search")
+        # register the internet search intent
+        register_intent(
+            intent_callback=self.internet_search_intent,
+            # We can add 'entities', (or named values to look for), to the intent.
+            intent_phrases=[
+                "Search (for | ) {search_phrase}", "Look up {search_phrase}"
+            ],
+            intent_name="Internet_search")
 
 
-def internet_search_intent(self, intent_data):
-    """This is a slightly fancier version of the hello world intent.
-            It takes a name as a parameter and says hello to that name."""
-    # get the name entity from the intent.
-    search_phrase = intent_data["entities"].get("search_phrase")
+    def internet_search_intent(self, intent_data):
+        """This is a slightly fancier version of the hello world intent.
+                It takes a name as a parameter and says hello to that name."""
+        # get the name entity from the intent.
+        search_phrase = intent_data["entities"].get("search_phrase")
 
-    self.audio_utils.say("Searching")
+        self.audio_utils.say("Searching")
 
-    url = f"https://www.google.com/search?q={search_phrase}"
+        url = f"https://www.google.com/search?q={search_phrase}"
 
-    webbrowser.open(url)
+        webbrowser.open(url)
