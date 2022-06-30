5d9de9f9467663bff6157a184f22effb7652697f
David Swearingen <captainD375@yahoo.com>
2022-06-30 16:56:43 -0600

    name fix

--- skills/internet_search_skill.py
+++ skills/internet_search_skill.py
@@ -11,34 +11,42 @@ class Skill(base_skill.BaseSkill):
     name = "Internet Search Skill"
 
 def __init__(self, settings_tool: SettingsTool, channels: Channels, audio_utils: AudioUtils):
-    self.settings_tool = settings_tool
-    self.channels = channels
-    self.audio_utils = audio_utils
+        self.settings_tool = settings_tool
+        self.channels = channels
+        self.audio_utils = audio_utils
 
 
 def intent_creator(self, register_intent: callable):
     """ This function registers intents using register_intent.
         Parameters: 
-            register_intent (callable): a function that can add an intent to Nora. """
+            register_intent (callable): a function that can add an intent to Nora. """   
 
     # register the internet search intent
-    register_intent(
-        intent_callback=self.internet_search_intent,
-        # We can add 'entities', (or named values to look for), to the intent.
-        intent_phrases=[
-            "Search (for | ) {search_phrase}", "Look up {search_phrase}"
-        ],
-        intent_name="Internet_search")
+    register_intent(intent_callback=self.internet_search_intent,
+                    # We can add 'entities', (or named values to look for), to the intent.
+                    intent_phrases=["Search (for | ) {search_phrase}", "Look up {search_phrase}"],
+                    intent_name="Internet_search")
 
 
 def internet_search_intent(self, intent_data):
-    """This is a slightly fancier version of the hello world intent.
+        """This is a slightly fancier version of the hello world intent.
             It takes a name as a parameter and says hello to that name."""
-    # get the name entity from the intent.
-    search_phrase = intent_data["entities"].get("search_phrase")
+        # get the name entity from the intent. 
+        search_phrase = intent_data["entities"].get("search_phrase")
+       
+        self.audio_utils.say("Searching")
+        
+        url = f"https://www.google.com/search?q={search_phrase}" 
 
-    self.audio_utils.say("Searching")
+        webbrowser.open(url)
 
-    url = f"https://www.google.com/search?q={search_phrase}"
 
-    webbrowser.open(url)
+
+
+
+
+
+
+
+
+
